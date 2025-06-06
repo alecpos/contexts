0d7e5f11fa99641837d4ed93eb8edad733a38f97
alecpos <127783896+alecpos@users.noreply.github.com>
2025-06-06 09:01:34 -0400

    jest

---
+++ jest.setup.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+// Read .env file
+const envPath = path.resolve(__dirname, '.env');
+const envConfig = fs.readFileSync(envPath, 'utf8')
+  .split('\n')
+  .reduce((acc, line) => {
+    const [key, value] = line.split('=');
+    if (key && value) {
+      acc[key.trim()] = value.trim();
+    }
+    return acc;
+  }, {});
+
+// Set environment variables
+Object.entries(envConfig).forEach(([key, value]) => {
+  process.env[key] = value;
+}); 
